8a6fdc02541aeabcd52a8ac26451cb1ea9a2a7c0
MAVHernandez <29807564+xelest@users.noreply.github.com>
2021-01-04 18:52:45 +0800

    editing
    editing now

---
+++ phpcore/core_registeruser.php
@@ -0,0 +1,64 @@
+<?php 
+  $con = mysqli_connect('localhost', 'root', '', 'cookmaster');
+
+  //on page load
+  $fname = "";
+  $lname = "";
+  $username = "";
+  $pwd1 = "";
+  $pwd2 = "";
+  $pwdN = "";
+
+
+ if (isset($_POST['register'])) {
+ 	$username = $_POST['username'];
+ 	$fname = $_POST['fname'];
+ 	$lname = $_POST['lname'];
+    $pwd1 = $_POST['pwd1'];
+    $pwd2 = $_POST['pwd2'];
+    $pwdN = passAjinomoto($pwd1);
+    $role = $_POST['role'];
+    $status = "deactivated";
+
+      $sql_u = "SELECT * FROM accounts WHERE username='$username'";
+      $res_u = mysqli_query($con, $sql_u);
+    
+      if (mysqli_num_rows($res_u) > 0) 
+      {
+        $name_error = "Sorry... username already taken"; 
+        echo "<script type='text/javascript'>alert('$name_error');</script>";
+      }
+      else
+      {
+        if ($pwd1 === $pwd2)
+        {
+           $query = "INSERT INTO accounts (`fname`, `lname`, `username`, `password`, `status`) 
+                    VALUES ('$fname','$lname','$username', '$pwdN', '$status')";
+                    
+           $results = mysqli_query($con, $query);
+           echo "<script type='text/javascript'>alert('Record created. Success!');</script>";
+           
+           exit();
+        }
+        else
+        {
+            echo "<script type='text/javascript'>alert('missmatched password!');</script>";
+        }
+  	}
+  }
+
+  function passAjinomoto($keypass){
+      $dbcon = mysqli_connect('localhost', 'root', '', 'cookmaster');
+      $p2 = $keypass;
+      $p1 = md5($p2);
+      $getQRY = mysqli_query($dbcon, "SELECT PASSWORD('$p1') as PWORD;");
+      while($rowX = $getQRY->fetch_assoc()){
+      $pwordX = $rowX['PWORD'];
+      }
+      $pwordX = md5($pwordX);
+      return $pwordX;
+    }
+
+
+
+?>
